c42cd74d0029e8db4d9d7d926de6dc7185882170
most <most@N4648.TM>
2019-02-05 16:43:18 +0200

    Technical fix

--- src/frontend/blocks/icon/index.js
+++ src/frontend/blocks/icon/index.js
@@ -3,7 +3,7 @@ import animate from 'GetwidUtils/animate';
 (function($){
 	$(document).ready(function(e){
 
-		$('.getwid-animation.wp-block-getwid-icon__wrapper').hover(function(){
+		$('.getwid-animation.wp-block-getwid-icon__wrapper').mouseenter(function(){
 			animate($(this), {
 				animation: $(this).attr('data-animation')
 			});
