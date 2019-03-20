c1eac8c8f70f78035efa8dd5721dcc8cc6ba123f
alexm <alexander.matienko@gmail.com>
2019-03-20 17:59:13 +0200

    * new block icons

--- src/blocks/testimonial/index.js
+++ src/blocks/testimonial/index.js
@@ -18,7 +18,7 @@ const {
 // Register the block
 registerBlockType('getwid/testimonial', {
 	title: __('Testimonial', 'getwid'),
-	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M24,9.3l-8.3-1.2L12,0.6L8.3,8.1L0,9.3l6,5.8l-1.4,8.2l7.4-3.9l7.4,3.9L18,15.1L24,9.3z M12,17.2l-4.7,2.5l0.9-5.3l-3.8-3.7 L9.6,10L12,5.1l2.4,4.9l5.3,0.8l-3.8,3.7l0.9,5.3L12,17.2z"/></svg>,
+	icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22,2v15h-4h-0.7l-0.6,0.5L12,21.4l-4.7-3.9L6.7,17H6H2V2H22 M24,0H0v19h6l6,5l6-5h6V0L24,0z"/><g><polygon points="6,6 6,11 8,11 8,14 9.5,12.5 11,11 11,6 	"/></g><g><polygon points="13,6 13,11 15,11 15,14 16.5,12.5 18,11 18,6 	"/></g></svg>,
 	category: 'getwid-blocks',
 	keywords: [
 	],
