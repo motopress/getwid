eb8bd9d1dc8791bfd0915824acf4d432a1605979
alexm <alexander.matienko@gmail.com>
2019-06-03 12:47:51 +0300

    * filter template load in admin

--- includes/blocks/template/post-content.php
+++ includes/blocks/template/post-content.php
@@ -1,6 +1,16 @@
 <?php
 
-function render_getwid_template_post_content( $attributes ) {
+function render_getwid_template_post_content( $attributes, $content ) {
+
+	// filter template load in admin
+	if ( is_admin() && get_post_type() == 'getwid_template_part') {
+		return $content;
+	}
+	// filter rest api - save
+	if ( defined( 'REST_REQUEST' ) && REST_REQUEST && (get_post_type() == 'getwid_template_part') ) {
+		return $content;
+	}
+
     $block_name = 'wp-block-getwid-template-post-content';
     $wrapper_class = $block_name;
 
@@ -56,15 +66,7 @@ function render_getwid_template_post_content( $attributes ) {
 		<?php if ( $attributes['showContent'] == 'excerpt' ) {
 			echo esc_html( wp_trim_words( get_the_excerpt(), $contentLength ) );
 		} elseif ($attributes['showContent'] == 'content'){
-            if (get_post_type() != 'getwid_template_part'){
-                if ($is_back_end){
-                    echo apply_filters( 'the_content', get_the_content(get_the_ID()) );                    
-                } else {
-                    the_content();
-                }
-            } else {
-                echo get_the_content();
-            }
+			the_content();
 		} elseif ($attributes['showContent'] == 'full'){
 			echo wp_kses_post( html_entity_decode( $current_post->post_content, ENT_QUOTES, get_option( 'blog_charset' ) ) );
 		} ?>
