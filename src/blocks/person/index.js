7c0b0fc28cd2f407b3b187b7714f6936f958ca64
aleandr100 <aleandr100@gmail.com>
2019-06-21 16:30:30 +0300

    - convert person/image;

--- src/blocks/person/index.js
+++ src/blocks/person/index.js
@@ -1,20 +1,21 @@
 /**
 * External dependencies
 */
+import { __ } from 'wp.i18n';
+
+/**
+* Internal dependencies
+*/
 import attributes from './attributes';
 import edit from './edit';
 import save from './save';
-import './style.scss'
 
+import './style.scss';
 
 /**
 * WordPress dependencies
 */
-import { __ } from 'wp.i18n';
-const {
-	registerBlockType,
-} = wp.blocks;
-
+const { registerBlockType, createBlock } = wp.blocks;
 
 /**
 * Register the block
@@ -25,10 +26,34 @@ registerBlockType('getwid/person', {
 	C22,17.8,19.4,14.2,15.7,12.7z M12,4c2.2,0,4,1.7,4,4c0,2.2-1.7,4-4,4s-4-1.7-4-4C8,5.7,9.8,4,12,4z M12,14c3.7,0,6.9,2.6,7.7,6H4.3	C5.1,16.6,8.3,14,12,14z"/></svg>,
 	category: 'getwid-blocks',
 	keywords: [
-		__('team', 'getwid'),
-		__('member', 'getwid'),
+		__( 'team'  , 'getwid' ),
+		__( 'member', 'getwid' )
 	],
-	attributes: attributes,
+	attributes,
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: [ 'core/image' ],
+				transform: content => ( createBlock( 'getwid/person', {
+					imgId: content.id,
+					imgUrl: content.url,
+					content: content.caption
+				} ) )
+			}
+		],
+		to: [
+			{
+				type: 'block',
+				blocks: [ 'core/image' ],
+				transform: attributes => ( createBlock( 'core/image', {
+					id: attributes.imgId,
+					url: attributes.imgUrl,
+					caption: attributes.content
+				} ) )
+			}
+		]
+	},
 	edit,
 	save
 });
