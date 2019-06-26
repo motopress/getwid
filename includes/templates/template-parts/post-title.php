0c4eff3059f5a68e257a5fe4879ff52597fb0656
Alexander Endzevich <endzevich@gmail.com>
2019-06-26 11:19:43 +0300

    * styling

--- includes/templates/template-parts/post-title.php
+++ includes/templates/template-parts/post-title.php
@@ -4,7 +4,7 @@
 extract($extra_attr);
 ?>
 
-<?php echo the_title(
+<?php the_title(
     '<'. esc_attr($headerTag) .(isset( $attributes['anchor'] ) ? ' id="'.esc_attr($attributes['anchor']).'" ' : '' ).(!empty($title_style) ? ' style="'.esc_attr($title_style).'"' : '') . (!empty($title_class) ? ' class="'.esc_attr($title_class).'"' : '').'>' .
     ($attributes['linkTo'] == 'post' ? '<a class="'.esc_attr($link_class).'" href="'.esc_url(get_permalink()).'">' : ''), ($attributes['linkTo'] == 'post' ? '</a>' : '').
     '</'. esc_attr($headerTag) .'>'
