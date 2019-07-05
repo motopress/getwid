6bebd8a780be1c9acab444cc5810cc51a298925d
alexm <alexander.matienko@gmail.com>
2019-07-05 17:44:02 +0300

    icons

--- src/blocks/template-parts/post-comments/index.js
+++ src/blocks/template-parts/post-comments/index.js
@@ -17,7 +17,7 @@ const { registerBlockType } = wp.blocks;
 */
 registerBlockType( 'getwid/template-post-comments', {
 	title: __( 'Post - Comments', 'getwid' ),
-	// icon: <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px"	viewBox="0 0 24 24"><path d="M0,0v10h10V0H0z M8,8H2V2h6V8z"/><rect x="12" y="2" width="12" height="2"/><rect x="12" y="6" width="8" height="2"/><path d="M0,14v10h10V14H0z M8,22H2v-6h6V22z"/><rect x="12" y="16" width="12" height="2"/><rect x="12" y="20" width="8" height="2"/></svg>,
+	icon: 'admin-comments',
 	category: (Getwid.settings.post_type == Getwid.templates.name ? 'getwid-post-blocks' : 'getwid-blocks'),
 	keywords: [ ],
 	supports: {
--- src/blocks/template-parts/post-meta/index.js
+++ src/blocks/template-parts/post-meta/index.js
@@ -18,7 +18,7 @@ const { InnerBlocks } = wp.editor;
 */
 registerBlockType( 'getwid/template-post-meta', {
 	title: __( 'Post - Meta', 'getwid' ),
-	// icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22,3v6H2V3H22 M24,1H0v10h24V1L24,1z"/><g><rect x="4" y="5" width="16" height="2"/></g><path d="M22,15v6H2v-6H22 M24,13H0v10h24V13L24,13z"/><g><rect x="4" y="17" width="16" height="2"/></g></svg>,
+	icon: 'tagcloud',
 	category: ( Getwid.settings.post_type == Getwid.templates.name ? 'getwid-post-blocks' : 'getwid-blocks' ),
 	keywords: [ ],
 	supports: {
