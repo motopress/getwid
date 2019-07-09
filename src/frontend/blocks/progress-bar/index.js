2430609e6ffe8b9a6e5303f7741c0b2b71f56777
DESKTOP-0DR7424\Serega <sereganfsmw@gmail.com>
2019-07-09 19:33:27 +0300

    Fixed progress bar line

--- src/frontend/blocks/progress-bar/index.js
+++ src/frontend/blocks/progress-bar/index.js
@@ -8,73 +8,36 @@
 			let className = '.wp-block-getwid-progress-bar',
 				$getwid_progress_bar = $(this),
 				getwid_fill_amount,
-				getwid_is_animated,
-
-				getwid_background_color,
-				getwid_text_color;
+				getwid_is_animated;
 
 			getwid_fill_amount = $getwid_progress_bar.find(`${className}__wrapper`).data('fill-amount');
 			getwid_is_animated = $getwid_progress_bar.find(`${className}__wrapper`).data('is-animated');
 
-			getwid_background_color = !!$getwid_progress_bar.find(`${className}__wrapper`).data('background-color') ? $getwid_progress_bar.find(`${className}__wrapper`).data('background-color') : '#eeeeee';
-			getwid_text_color 		= !!$getwid_progress_bar.find(`${className}__wrapper`).data('text-color') ? $getwid_progress_bar.find(`${className}__wrapper`).data('text-color') : '#0000ee';
-		
-			function setSize() {		
-				const canvas = $getwid_progress_bar.find(`${className}__canvas`).get(0);
-				const width  = $getwid_progress_bar.find(`${className}__wrapper`).css('width');
-		
-				canvas.width = parseFloat(width.replace(/px$/, ''));
-				canvas.height = 10;
-		
-				return canvas.width;
-			}
-
 			function drawLines(value) {
-
-				const context = $getwid_progress_bar.find(`${className}__canvas`).get(0).getContext('2d');
-		
-				let width = setSize();
-				context.clearRect(0, 0, width, 10);
-		
-				context.beginPath();
-		
-				context.moveTo(0, 10);
-				context.lineTo(width, 10);
-		
-				context.lineWidth = 10;
-				context.strokeStyle = getwid_background_color;
-				context.stroke();
-		
-				context.beginPath();
-		
-				context.moveTo(0, 10);
-				context.lineTo((width * value) / 100, 10);
-		
-				context.lineWidth = 10;
-				context.strokeStyle = getwid_text_color;
-				context.stroke();
-		
-				$getwid_progress_bar.find(`${className}__percent`).html(`${value}%`);
+				$getwid_progress_bar.find($(`${className}__progress`)).css('width', `${value}%`);
+				$getwid_progress_bar.find($(`${className}__percent`)).text(`${value}%`);
 			}
 
-			function drawAnimatedLines() {
-				let value = 0;
-				let fill = setInterval(() => {
-					drawLines(value);
+			function drawAnimatedLines(value) {
+				let $content = $getwid_progress_bar.find($(`${className}__progress`));
 		
-					value++;
-					if (value > getwid_fill_amount) {
-						clearInterval(fill);
-					}
-				}, 35);
+				const percent = () => { return Math.round(($content.width() / $content.parent().width()) * 100); }
+		
+				$content.animate({ width: `${value}%` }, {
+					duration: 2000,
+					progress: () => {
+						let $percent =  $getwid_progress_bar.find($(`${className}__percent`));
+						$percent.text(percent() + '%');
+					},
+				});
 			}
 
-			const $bar = $getwid_progress_bar.find($(`${className}__line-wrapper`));
+			const $bar = $getwid_progress_bar.find($(`${className}__wrapper`));
 
 			const waypoint = new Waypoint({
 				element: $bar.get(0), handler: () => {					
 					if (getwid_is_animated) {
-						drawAnimatedLines();
+						drawAnimatedLines(getwid_fill_amount);
 					} else {
 						drawLines(getwid_fill_amount);
 					}
