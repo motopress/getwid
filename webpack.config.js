38306a93d136cd76c6ef67b7070360b7424025ed
aleandr100 <aleandr100@gmail.com>
2019-07-22 16:14:24 +0300

    - delete old dependencies from package & add report option;

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require( 'extract-text-webpack-plugin' );
 
 /* #region include new plugin */
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require( 'webpack-bundle-analyzer' ).BundleAnalyzerPlugin;
 /* #endregion */
 
 // Set different CSS extraction for editor only and common block styles
-const blocksCSSPlugin = new ExtractTextPlugin({
+const blocksCSSPlugin = new ExtractTextPlugin( {
 	filename: './assets/css/blocks.style.css'
-});
-const editBlocksCSSPlugin = new ExtractTextPlugin({
+} );
+const editBlocksCSSPlugin = new ExtractTextPlugin( {
 	filename: './assets/css/blocks.editor.css'
-});
+} );
 
-/* #region create instance */
-const bundleAnalyzerPlugin = new BundleAnalyzerPlugin({
-	analyzerMode: 'server',
+// Webpack Bundle Analyzer
+const bundleAnalyzerPlugin = new BundleAnalyzerPlugin( {
+	analyzerMode: 'disabled',
 	generateStatsFile: true,
 	statsOptions: { source: false }
-});
-/* #endregion */
+} );
 
 // Configuration for the ExtractTextPlugin.
 const extractConfig = {
@@ -30,7 +29,7 @@ const extractConfig = {
 		{
 			loader: 'postcss-loader',
 			options: {
-				plugins: [require('autoprefixer')]
+				plugins: [ require('autoprefixer') ]
 			}
 		},
 		{
@@ -45,7 +44,7 @@ const extractConfig = {
 
 const config = {
 	entry: {
-		'./assets/js/editor.blocks': './src/index.js',
+		'./assets/js/editor.blocks'  : './src/index.js',
 		'./assets/js/frontend.blocks': './src/frontend.js'
 	},
 	output: {
@@ -55,7 +54,7 @@ const config = {
 		libraryTarget: 'window',
 	},
 	devtool: 'production' !== process.env.NODE_ENV ? 'cheap-eval-source-map' : false,
-	watch: 'production' !== process.env.NODE_ENV,
+	watch  : 'production' !== process.env.NODE_ENV,
 	stats: { children: false },
 	module: {
 		rules: [
@@ -82,14 +81,14 @@ const config = {
 		'lodash': 'lodash',
 		//https://www.cssigniter.com/importing-gutenberg-core-wordpress-libraries-es-modules-blocks/
 		'wp.i18n': {
-			window: ['wp', 'i18n'],
+			window: ['wp', 'i18n']
 		},
 	},
 	resolve: {
 		alias: {
-			GetwidControls: path.resolve(__dirname, 'src/controls/'),
-			GetwidUtils: path.resolve(__dirname, 'src/utils/'),
-			GetwidVendor: path.resolve(__dirname, 'vendors/'),
+			GetwidControls: path.resolve( __dirname, 'src/controls/'),
+			GetwidUtils   : path.resolve( __dirname, 'src/utils/'   ),
+			GetwidVendor  : path.resolve( __dirname, 'vendors/'     )
 		}
 	},
 	plugins: [
