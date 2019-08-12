13e4e2859caf88a60f0089642baed8de5e19e68b
DESKTOP-0DR7424\Serega <sereganfsmw@gmail.com>
2019-08-12 23:49:23 +0300

    Icon Move on Translate [BUG]

--- includes/scripts-manager.php
+++ includes/scripts-manager.php
@@ -658,6 +658,28 @@ class ScriptsManager {
 	public function enqueueScriptsAndStyles(){
 		//Scripts
 		wp_enqueue_script(
+			'gsap-draggable',
+			getwid_get_plugin_url( 'vendors/draggable/Draggable.min.js' ),
+			[ 'jquery', 'gsap-css-plugin', 'gsap-tween-lite' ],
+			'2.1.3',
+			true
+		);
+		wp_enqueue_script(
+			'gsap-css-plugin',
+			getwid_get_plugin_url( 'vendors/draggable/CSSPlugin.min.js' ),
+			[ 'jquery' ],
+			'2.1.3',
+			true
+		);
+		wp_enqueue_script(
+			'gsap-tween-lite',
+			getwid_get_plugin_url( 'vendors/draggable/TweenLite.min.js' ),
+			[ 'jquery' ],
+			'2.1.3',
+			true
+		);		
+
+		wp_enqueue_script(
 			'jquery-countdown',
 			getwid_get_plugin_url( 'vendors/jquery.countdown/jquery.countdown.min.js' ),
 			[ 'jquery', 'jquery-plugin' ],
@@ -812,6 +834,10 @@ class ScriptsManager {
 				'jquery-ui-tabs',
 				'jquery-ui-accordion',
 				'jquery-ui-draggable',
+
+				'gsap-draggable',
+				'gsap-css-plugin',
+				'gsap-tween-lite',
 			],
 			$this->version,
 			true
