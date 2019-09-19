9b38d86c685de620dca2e334244be3be5a97b9d8
aleandr100 <aleandr100@gmail.com>
2019-09-19 16:21:25 +0300

    - mailchimp hot fix;

--- includes/templates/mailchimp/field-first-name.php
+++ includes/templates/mailchimp/field-first-name.php
@@ -4,18 +4,20 @@
     if ( isset( $attributes[ 'className' ] ) ) {
         $class .= ' ' . $attributes[ 'className' ];
     }
-	$uid  = isset( $attributes[ 'id'   ] ) ? esc_attr( $attributes[ 'id' ] ) : 'name-' . uniqid();
-    $name = isset( $extra_attr[ 'name' ] ) ? $extra_attr[ 'name' ]           : 'name';
-    $label = $attributes[ 'label' ];
+	$uid = isset( $attributes[ 'id'   ] ) ? esc_attr( $attributes[ 'id' ] ) : 'name-' . uniqid();
 ?>
-<p class='<?php echo esc_attr( $class );?>'>
-    <label
-        for='<?php echo $uid ?>'
-        class='<?php echo esc_attr( $block_name . '__label' );?>'
-    ><?php
-        echo $label;
-    ?></label>
-    <input id='<?php echo $uid ?>' type='text' name='<?php echo $name ?>'<?php
+<p class='<?php echo esc_attr( $class );?>'><?php
+    if ( ! empty( $attributes[ 'label' ] ) ) {?>
+        <label
+		    for='<?php echo $uid ?>'
+            class='<?php echo esc_attr( $block_name . '__label' );?>'
+        ><?php
+            echo $attributes[ 'label' ];
+        ?></label>
+        <?php
+    }?>
+    
+    <input id='<?php echo $uid ?>' type='text' name='first-name'<?php
         if ( isset( $attributes[ 'placeholder' ] ) ) { ?>
             placeholder='<?php echo esc_attr( $attributes[ 'placeholder' ] ); ?>' <?php
         } ?>
