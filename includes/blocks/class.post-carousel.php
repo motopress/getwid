c059f9172d203f44103ab47336e7a7685772f14b
aleandr100 <aleandr100@gmail.com>
2019-10-03 14:48:53 +0300

    - exclude blocks;

--- includes/blocks/class.post-carousel.php
+++ includes/blocks/class.post-carousel.php
@@ -8,63 +8,63 @@ class PostCarousel {
 
     public function __construct() {
 
-        add_filter( 'getwid/editor_blocks_js/dependencies', [ $this, 'block_editor_scripts'] );
-        add_filter( 'getwid/editor_blocks_css/dependencies', [ $this, 'block_editor_styles' ] );
+        add_filter( 'getwid/editor_blocks_js/dependencies' , [ $this, 'block_editor_scripts' ] );
+        add_filter( 'getwid/editor_blocks_css/dependencies', [ $this, 'block_editor_styles'  ] );
 
         register_block_type(
             $this->blockName,
             array(
                 'attributes' => array(
                     'postTemplate' => array(
-                        'type' => 'string',
+                        'type' => 'string'
                     ),              
         
                     //Custom Post Type
                     'postsToShow' => array(
                         'type' => 'number',
-                        'default' => 5,
+                        'default' => 5
                     ),     
                     'ignoreSticky' => array(
                         'type' => 'boolean',
-                        'default' => true,
+                        'default' => true
                     ),      
                     'filterById' => array(
-                        'type' => 'string',
+                        'type' => 'string'
                     ),      
                     'parentPageId' => array(
-                        'type' => 'string',
+                        'type' => 'string'
                     ),                                        
                     'postType' => array(
                         'type' => 'string',
-                        'default' => 'post',
+                        'default' => 'post'
                     ),
                     'taxonomy' => array(
                         'type' => 'array',
                         'items'   => [
-                            'type' => 'string',
+                            'type' => 'string'
                         ],
                     ),            
                     'terms' => array(
                         'type' => 'array',
                         'items'   => [
-                            'type' => 'string',
+                            'type' => 'string'
                         ],
                     ),
                     'relation' => array(
                         'type' => 'string',
-                        'default' => 'AND',
+                        'default' => 'AND'
                     ),
                     'order' => array(
                         'type' => 'string',
-                        'default' => 'desc',
+                        'default' => 'desc'
                     ),
                     'orderBy' => array(
                         'type' => 'string',
-                        'default' => 'date',
+                        'default' => 'date'
                     ),
                     //Custom Post Type
                     'align' => array(
-                        'type' => 'string',
+                        'type' => 'string'
                     ),
         
                     //Slider
@@ -120,10 +120,9 @@ class PostCarousel {
                         'type' => 'string',
                         'default' => 'ouside'
                     ),     
-                    
                     'className' => array(
-                        'type' => 'string',
-                    ),              
+                        'type' => 'string'
+                    )
                 ),
                 'render_callback' => [ $this, 'render_block' ]
             )
@@ -200,7 +199,7 @@ class PostCarousel {
     public function render_block( $attributes, $content ) {
         //Custom Post Type
         $query_args = [];
-        getwid_build_custom_post_type_query($query_args, $attributes);
+        getwid_build_custom_post_type_query( $query_args, $attributes );
 
         $q = new \WP_Query( $query_args );
         //Custom Post Type
@@ -209,81 +208,86 @@ class PostCarousel {
         $use_template = false;
         $template_part_content = '';
         
-        if ( isset( $attributes['postTemplate'] ) && $attributes['postTemplate'] != '' ) {
+        if ( isset( $attributes[ 'postTemplate' ] ) && $attributes[ 'postTemplate' ] != '' ) {
 
-            $template_post = get_post($attributes['postTemplate'], ARRAY_A);
+            $template_post = get_post( $attributes[ 'postTemplate' ], ARRAY_A );
 
             //If post exist and content not empty
-            if (!is_null($template_post) && $template_post['post_content'] != ''){
+            if ( ! is_null( $template_post ) && $template_post[ 'post_content' ] != '' ) {
                 $use_template = true;
-                $template_part_content = $template_post['post_content'];
+                $template_part_content = $template_post[ 'post_content' ];
             }
         }
 
         $block_name = 'wp-block-getwid-post-carousel';
 
-        $post_type =  isset($attributes['postType']) ? $attributes['postType'] : 'post';
+        $post_type =  isset( $attributes[ 'postType' ] ) ? $attributes[ 'postType' ] : 'post';
 
         $extra_attr = array(
-            'block_name' => $block_name,
+            'block_name' => $block_name
         );
 
         $class = $block_name;
-        $class .= ' custom-post-type-' . esc_attr($post_type);
+        $class .= ' custom-post-type-' . esc_attr( $post_type );
 
-        if ( isset( $attributes['align'] ) ) {
-            $class .= ' align' . $attributes['align'];
+        if ( isset( $attributes[ 'align' ] ) ) {
+            $class .= ' align' . $attributes[ 'align' ];
         }
 
-        if ( isset( $attributes['showPostDate'] ) && $attributes['showPostDate'] ) {
+        if ( isset( $attributes[ 'showPostDate' ] ) && $attributes[ 'showPostDate' ] ) {
             $class .= ' has-dates';
         }
-        if ( isset( $attributes['className'] ) ) {
-            $class .= ' ' . esc_attr($attributes['className']);
+        if ( isset( $attributes[ 'className' ] ) ) {
+            $class .= ' ' . esc_attr( $attributes[ 'className' ] );
         }
 
         $wrapper_class = esc_attr($block_name).'__wrapper';
 
         $wrapper_class .= " no-init-slider";
 
-        if ( isset( $attributes['sliderSlidesToShowDesktop'] ) && $attributes['sliderSlidesToShowDesktop'] > 1 ) {
-            $class .= ' has-slides-gap-'.esc_attr($attributes['sliderSpacing']);
+        if ( isset( $attributes[ 'sliderSlidesToShowDesktop' ] ) && $attributes[ 'sliderSlidesToShowDesktop' ] > 1 ) {
+            $class .= ' has-slides-gap-' . esc_attr( $attributes[ 'sliderSpacing' ] );
             $class .= ' is-carousel';
         }
 
-        $class .= ' has-arrows-'.esc_attr($attributes['sliderArrows']);
-        $class .= ' has-dots-'.esc_attr($attributes['sliderDots']);
+        $class .= ' has-arrows-' . esc_attr( $attributes[ 'sliderArrows' ] );
+        $class .= ' has-dots-'   . esc_attr( $attributes[ 'sliderDots'   ] );
 
         $sliderData = array(
-            'sliderSlidesToShowDesktop' => $attributes['sliderSlidesToShowDesktop'],
-            'getwid_slidesToShowLaptop' => $attributes['sliderSlidesToShowLaptop'],
-            'getwid_slidesToShowTablet' => $attributes['sliderSlidesToShowTablet'],
-            'getwid_slidesToShowMobile' => $attributes['sliderSlidesToShowMobile'],
-            'getwid_slidesToScroll' => $attributes['sliderSlidesToScroll'],
-            'getwid_autoplay' => $attributes['sliderAutoplay'],
-            'getwid_autoplay_speed' => intval($attributes['sliderAutoplaySpeed']),
-            'getwid_infinite' => $attributes['sliderInfinite'],
-            'getwid_animation_speed' => intval($attributes['sliderAnimationSpeed']),
-            'getwid_center_mode' => $attributes['sliderCenterMode'],
-            'getwid_arrows' => $attributes['sliderArrows'],
-            'getwid_dots' => $attributes['sliderDots'],
+            'sliderSlidesToShowDesktop' => $attributes[ 'sliderSlidesToShowDesktop' ],
+            'getwid_slidesToShowLaptop' => $attributes[ 'sliderSlidesToShowLaptop'  ],
+            'getwid_slidesToShowTablet' => $attributes[ 'sliderSlidesToShowTablet'  ],
+            'getwid_slidesToShowMobile' => $attributes[ 'sliderSlidesToShowMobile'  ],
+
+            'getwid_autoplay_speed'  => intval( $attributes[ 'sliderAutoplaySpeed'  ] ),
+            'getwid_animation_speed' => intval( $attributes[ 'sliderAnimationSpeed' ] ),
+
+            'getwid_slidesToScroll'  => $attributes[ 'sliderSlidesToScroll'],
+            'getwid_autoplay'        => $attributes[ 'sliderAutoplay'      ],
+            'getwid_infinite'        => $attributes[ 'sliderInfinite'      ],
+            
+            'getwid_center_mode'     => $attributes[ 'sliderCenterMode' ],
+            'getwid_arrows'          => $attributes[ 'sliderArrows'     ],
+            'getwid_dots'            => $attributes[ 'sliderDots'       ]
         );
 
-        $slider_options = json_encode($sliderData);
+        $slider_options = json_encode( $sliderData );
         ob_start();
         ?>
 
         <div class="<?php echo esc_attr( $class ); ?>">
-            <div data-slider-option="<?php echo esc_attr($slider_options); ?>" class="<?php echo esc_attr( $wrapper_class );?>">
+            <div data-slider-option="<?php echo esc_attr( $slider_options ); ?>" class="<?php echo esc_attr( $wrapper_class );?>">
                 <?php
 
-                if ( !$use_template ) {
+                if ( ! $use_template ) {
                     $template = $post_type;
                     $located = getwid_locate_template( 'post-carousel/' . $post_type );
-                    if ( !$located ) {
+                    if ( ! $located ) {
                         $template = 'post';
                     }
                 }
+
+                $block_exclude = array( 'post-carousel', 'map', 'image-hotspot' );
 
                 if ( $q->have_posts() ):
                     ob_start();
@@ -291,19 +295,24 @@ class PostCarousel {
                     while( $q->have_posts() ):
                         $q->the_post();
 
-                        $current_block = strpos( get_the_content(), 'post-carousel' );
+                        $has_exclude_block = false;
+                        foreach( $block_exclude as $key => $value ) {
+                            if ( strpos( get_the_content(), $value ) ) {
+                                $has_exclude_block = true;
+                            }
+                        }
                                                 
-                        if ($use_template){
-                            if ( ! $current_block ) {
+                        if ( $use_template ) {
+                            if ( ! $has_exclude_block ) {
                             ?>
-                                <div class="<?php echo esc_attr($block_name);?>__slide">
+                                <div class="<?php echo esc_attr( $block_name );?>__slide">
                                     <?php echo do_blocks( $template_part_content ); ?>
                                 </div>
                             <?php
                             }
                         } else {
                             ?>
-                                <div class="<?php echo esc_attr($block_name);?>__slide">
+                                <div class="<?php echo esc_attr( $block_name );?>__slide">
                                     <?php getwid_get_template_part( 'post-carousel/' . $template, $attributes, false, $extra_attr ); ?>
                                 </div>
                             <?php
