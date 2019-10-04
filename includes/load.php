3bdedc32e60d14acc7e66b8d8c4a8907887336a3
aleandr100 <aleandr100@gmail.com>
2019-10-04 10:47:16 +0300

    - load.php class prefix deleted;

--- includes/load.php
+++ includes/load.php
@@ -6,17 +6,17 @@ if ( !defined( 'ABSPATH' ) ) {
 
 require_once dirname( __FILE__ ) . '/functions.php';
 require_once dirname( __FILE__ ) . '/translation.php';
-require_once dirname( __FILE__ ) . '/class.settings.php';
-require_once dirname( __FILE__ ) . '/class.getwid.php';
-require_once dirname( __FILE__ ) . '/class.scripts-manager.php';
-require_once dirname( __FILE__ ) . '/font-icons-manager/class.font-icons-manager.php';
-require_once dirname( __FILE__ ) . '/class.blocks-manager.php';
-require_once dirname( __FILE__ ) . '/class.version-control.php';
-require_once dirname( __FILE__ ) . '/class.writing-settings.php';
-require_once dirname( __FILE__ ) . '/class.rest-api.php';
-require_once dirname( __FILE__ ) . '/class.post-template-part.php';
-require_once dirname( __FILE__ ) . '/class.mailer.php';
-require_once dirname( __FILE__ ) . '/class.allowed-css-tags.php';
+require_once dirname( __FILE__ ) . '/settings.php';
+require_once dirname( __FILE__ ) . '/getwid.php';
+require_once dirname( __FILE__ ) . '/scripts-manager.php';
+require_once dirname( __FILE__ ) . '/font-icons-manager/font-icons-manager.php';
+require_once dirname( __FILE__ ) . '/blocks-manager.php';
+require_once dirname( __FILE__ ) . '/version-control.php';
+require_once dirname( __FILE__ ) . '/writing-settings.php';
+require_once dirname( __FILE__ ) . '/rest-api.php';
+require_once dirname( __FILE__ ) . '/post-template-part.php';
+require_once dirname( __FILE__ ) . '/mailer.php';
+require_once dirname( __FILE__ ) . '/allowed-css-tags.php';
 
 if ( class_exists('\DrewM\MailChimp\MailChimp') === false ) {
 	require_once dirname( __FILE__ ) . '/libraries/mailchimp-api/src/MailChimp.php';
