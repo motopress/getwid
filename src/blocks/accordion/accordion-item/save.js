4de89a8b4f0256673ed582e8c3f25aee6fddbdab
Serega MoST <sereganfsmw.nod@gmail.com>
2020-05-28 23:00:33 +0300

    - tech fix

--- src/blocks/accordion/accordion-item/save.js
+++ src/blocks/accordion/accordion-item/save.js
@@ -20,46 +20,9 @@ class Save extends Component {
 		const {className, baseClass} = this.props;
 		const {outerParent, title} = this.props.attributes;
 
-		const itemClass= {
-			className: classnames(className, {
-			})
-		};
-
 		const Tag = outerParent ? outerParent.attributes.headerTag : 'span';
 		const iconOpen = outerParent ? outerParent.attributes.iconOpen : 'fas fa-minus';
 		const iconClose = outerParent ? outerParent.attributes.iconClose : 'fas fa-plus';
-
-
-
-		// return (
-		// 	<div {...itemClass}>
-		// 		<div className={`${baseClass}__header-wrapper`}>
-		// 			{title && (
-		// 				<Tag className={`${baseClass}__header`}>
-		// 					<a href="#">
-		// 						<RichText.Content
-		// 							tagName={'span'}
-		// 							className={`${baseClass}__header-title`}
-		// 							value={title}
-		// 						/>
-		// 						<span className={`${baseClass}__icon is-active`}><i className={iconClose}></i></span>
-		// 						<span className={`${baseClass}__icon is-passive`}><i className={iconOpen}></i></span>
-		// 					</a>
-		// 				</Tag>
-		// 			)}
-		// 		</div>
-
-		// 		<div className={`${baseClass}__content-wrapper`}>
-		// 			<div className={`${baseClass}__content`}>
-		// 				<InnerBlocks.Content/>
-		// 			</div>
-		// 		</div>
-		// 	</div>
-		// );
-
-
-
-
 
 		return (
 			<Fragment>
