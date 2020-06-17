7990eccd8269432b4a48375bfc66a95b442cba48
Serega MoST <sereganfsmw.nod@gmail.com>
2020-06-17 19:06:43 +0300

    - fix "Countdown" Mac OS date format

--- src/blocks/countdown/inspector.js
+++ src/blocks/countdown/inspector.js
@@ -48,7 +48,8 @@ class Inspector extends Component {
 		const { fontStyle, textTransform, lineHeight, letterSpacing, backgroundColor, innerPadding, innerSpacings } = this.props.attributes;
 		const { setTextColor,textColor, setAttributes } = this.props;
 
-		const defaultDate = new Date( Getwid.settings.date_time_utc );
+		const defaultDate = new Date( Getwid.settings.date_time_utc.replace(/-/g, "/") );
+
 		defaultDate.setDate( defaultDate.getDate() + 1 );
 
 		const { tabName } = this.state;
