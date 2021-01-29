079801f0e4eb8f17a5598f2bcd9677f6388056b7
SoftWebIX <SoftWebIX>
2021-01-29 16:11:18 +0200

    fix weight

--- src/controls/google-fonts-control/index.js
+++ src/controls/google-fonts-control/index.js
@@ -188,6 +188,24 @@ class GoogleFontsControl extends Component {
 						__( 'Loading…', 'getwid' )
 					) }
 				</BaseControl>
+
+				<SelectControl
+					label={ __( 'Font Weight', 'getwid' ) }
+					value={ this.props.valueWeight || '' }
+					options={ this.state.variants ? this.state.variants : [
+						{value: '', label: __('Default', 'getwid')},
+						{value: '100', label: '100'},
+						{value: '200', label: '200'},
+						{value: '300', label: '300'},
+						{value: 'normal', label: '400'},
+						{value: '500', label: '500'},
+						{value: '600', label: '600'},
+						{value: '700', label: '700'},
+						{value: '800', label: '800'},
+						{value: '900', label: '900'},
+					] }
+					onChange={ this.props.onChangeFontWeight }
+				/>
 			</div>
 		);
 	}
