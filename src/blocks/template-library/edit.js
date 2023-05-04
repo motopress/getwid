d07b82b076bf5d70006d90b8221a9e38d574bb37
Olexandr Yendzevych <endzevich@gmail.com>
2023-05-04 15:51:20 +0300

    add missed keys for template library items render

--- src/blocks/template-library/edit.js
+++ src/blocks/template-library/edit.js
@@ -245,7 +245,7 @@ class Edit extends Component {
 				return item.map((key, index) => {
 
 					return (
-						<div className="template-library-container">
+						<div className="template-library-container" key={index}>
 							<div
 								className="template-library-item"
 								onClick={
@@ -295,16 +295,14 @@ class Edit extends Component {
 
 							{Object.keys(pageTemplatesArr).map((category_key, index) => {
 								return (
-									<Fragment>
-										<div className="template-library-category-wrapper">
-											<div className="template-library-category-title">
-												<h2>{(typeof pageCategories[type] != 'undefined' ? pageCategories[type][category_key] : undefined)}</h2>
-											</div>
-											<div className="template-library-category-items">
-												{renderSingleItem(pageTemplatesArr[category_key])}
-											</div>
+									<div className="template-library-category-wrapper" key={index}>
+										<div className="template-library-category-title">
+											<h2>{(typeof pageCategories[type] != 'undefined' ? pageCategories[type][category_key] : undefined)}</h2>
 										</div>
-									</Fragment>
+										<div className="template-library-category-items">
+											{renderSingleItem(pageTemplatesArr[category_key])}
+										</div>
+									</div>
 								);
 							})}
 						</Fragment>
